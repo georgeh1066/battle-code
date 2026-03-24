76f0629549d589c10712e3028d2de7c1826bc78d
jhescot <joshua.hescot@gmail.com>
2026-03-24 18:59:20 +0000

    bots going to enemy core

--- bots/jbot/main.py
+++ bots/jbot/main.py
@@ -20,11 +20,14 @@ class Player:
     def __init__(self):
         self.num_spawned = 0
         self.target = (0, 0)
+        self.prev_targets = []
+        self.enemy_core_list = []  # list of possible enemy core positions based on our core position and map size, for builder bots to target if we haven't found any ore yet
         self.core_pos = (0, 0)
         self.ti_poses = []  # list of known titanium ore positions
         self.ax_poses = []  # list of known axionite ore positions
         self.wall_poses = []  # list of known wall positions
         self.searched = False  # whether we've done a search for ore yet (for the core)
+        self.change_target = True  # whether builder bots should change target.
 
     def run(self, ct: Controller) -> None:
         etype = ct.get_entity_type()
@@ -58,6 +61,14 @@ class Player:
 
         elif etype == EntityType.BUILDER_BOT:
 
+            if not self.searched:
+                for unit in ct.get_nearby_units(1):
+                    if ct.get_entity_type(unit) == EntityType.CORE:
+                        self.core_pos = ct.get_position(unit)
+                        self.enemy_core_list = self.find_core(self.core_pos, (ct.get_map_width(), ct.get_map_height()))
+                        self.searched = True
+                
+
             # Check nearby tiles for ore and record their positions.
 
             for tile in ct.get_nearby_tiles():
@@ -67,18 +78,38 @@ class Player:
                     self.ax_poses.append(tile)
                 if ct.get_tile_env(tile).name == "WALL":
                     self.wall_poses.append(tile)
-            
-            # set target to the closest known titanium ore position, and move towards it, building a conveyor if possible. If we are adjacent to the target ore, build a harvester on it. Finally, place a marker in a random adjacent tile.
+                if tile == self.target:
+                    building_id = ct.get_tile_building_id(tile)
+                    if ct.get_entity_type(building_id) != EntityType.CORE:
+                        self.change_target = True
 
-            self.target = self.ti_poses[0]
+            if self.change_target:
+                for i in range(len(self.enemy_core_list)):
+                    if self.enemy_core_list[i] not in self.prev_targets:
+                        self.target = self.enemy_core_list[i]
+                        self.prev_targets.append(self.target)
+                        self.change_target = False
+                        break
             dir = ct.get_position().direction_to(self.target)
 
             
             move_pos = ct.get_position().add(dir)
-            if ct.can_build_conveyor(move_pos, dir.opposite()):
-                ct.build_conveyor(move_pos, dir.opposite())
             if ct.can_move(dir):
                 ct.move(dir)
+            if ct.can_build_road(move_pos):
+                ct.build_road(move_pos)
+            else:
+                dir = dir.rotate_right()
+                move_pos = ct.get_position().add(dir)
+                if ct.can_move(dir):
+                    ct.move(dir)
+                if ct.can_build_road(move_pos):
+                    ct.build_road(move_pos)
+                else:
+                    dir = dir.rotate_right()
+                    move_pos = ct.get_position().add(dir)
+                    if ct.can_move(dir):
+                        ct.move(dir)
 
             for d in Direction:
                 check_pos = ct.get_position().add(d)
@@ -89,3 +120,15 @@ class Player:
             marker_pos = ct.get_position().add(random.choice(DIRECTIONS))
             if ct.can_place_marker(marker_pos):
                 ct.place_marker(marker_pos, marker_pos[0])
+    
+    
+    def find_core(self, cpos, map_size):
+
+        #finds the 3 possible enemy core positions based on allied core position and map size.
+
+        horiz_pos = Position(map_size[0]-1-cpos[0], cpos[1])
+        vert_pos = Position(cpos[0], map_size[1]-1-cpos[1])
+        rot_pos = Position(map_size[0]-1-cpos[0], map_size[1]-1-cpos[1])
+        candidates = [horiz_pos, vert_pos, rot_pos]
+
+        return candidates
